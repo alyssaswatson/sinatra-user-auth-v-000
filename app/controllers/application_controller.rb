2a44b6a5da6de75cc74e879f982b27b3bfb3c2cf
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-12 22:00:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
+    #puts params
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    puts params
+    #puts params
     redirect '/users/home'
   end
 
